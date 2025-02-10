d3512f5c67587c5a45827f8c82327237c3a77a54
Bernie Truong <tqtolympia@gmail.com>
2025-02-10 14:48:31 +0700

    fix: temporarily omit encryption password for admin site
    and fix miss viewCustomer servlet at web.xml

--- src/java/context/DBContext.java
+++ src/java/context/DBContext.java
@@ -13,8 +13,8 @@ public abstract class DBContext<T> {
     public DBContext()
     {
         try {
-            String user = "hungdv";
-            String pass = "12345";
+            String user = "sa";
+            String pass = "123";
             String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=swp391_v1;trustServerCertificate=true;";
             
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
