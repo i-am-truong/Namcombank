9aa94045a93599ffd9aac68f9fc69e996030a0d6
Bernie Truong <tqtolympia@gmail.com>
2025-02-11 15:57:24 +0700

    feat: modify edit customer function at admin side
    still fixing .gitignore file

--- src/java/controller/customer/addCustomer.java
+++ src/java/controller/customer/addCustomer.java
@@ -49,36 +49,6 @@ public class addCustomer extends BaseRBACControlller {
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
-        
-    }
-
     /**
      * Returns a short description of the servlet.
      *
